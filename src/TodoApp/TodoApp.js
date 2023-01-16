7782df8b14ae512878b396d75b50f0dd1e55937f
ASHFAK <ashfakv17@gmail.com>
2023-01-16 22:44:04 +0530

    Iteam addedd

--- src/TodoApp/TodoApp.js
+++ src/TodoApp/TodoApp.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react'
 import "./TodoApp.css"
 export default class TodoApp extends Component {
+
+    state={
+        inputText :"",
+        iteams:[]
+    };
+
+handlechange=(event)=>{
+ this.setState({
+    inputText : event.target.value
+ })
+ 
+};
+
+storeIteams =(event) =>{
+    event.preventDefault()
+const {inputText} =this.state;
+
+
+this.setState({
+    iteams:[...this.state.iteams,inputText]
+    
+})
+}
+
+
+
   render() {
+
+    const {inputText,iteams} = this.state
+   
     return (
       <div className='Todo-Container'>
        
 
-     
-
-
-        <div className='input-section'>  
+        <form className='input-section' onSubmit={this.storeIteams}>  
           <h1>Todo APP</h1>
-            <input type="text" style={{fontSize:20}} placeholder="Enter Iteams...." />
+            <input type="text" 
+         value={inputText}
+           style={{fontSize:20}} 
+           onChange={this.handlechange}
+             placeholder="Enter Iteams...." />
             
       
-        </div>
+        </form>
+          
+
         <ul>
-            <li>iteams</li>
-            <li>iteams</li>
-        </ul>
-      </div>
+     {iteams.map((data, index)=>(
+
+      
+        <li key={index}>{data}<i className='fa fa-trash'></i></li>  
+
+  ))}
+  
+
+        </ul> 
+      </div>   
     )
   }
 }
